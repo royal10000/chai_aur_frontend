a71c9c68ddd24fd281359fae8e8d2825611c3360
royal10000 <lurey100000@gmail.com>
2024-03-20 19:27:17 +0545

    understand the react flow and structure

---
+++ chai_aur_frontend/src/Hell.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Hell = function () {
+    return (
+        <div>he</div>
+    )
+}
--- chai_aur_frontend/src/app.jsx
+++ chai_aur_frontend/src/app.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'preact/hooks'
-
+// import { useState }/ from 'preact/hooks'
+// import h from './he'
+import { Hell } from "./Hell"
 
 export function App() {
 
   return (
     <>
       <h1>Chai with react with vite | Thuman Rana</h1>
+      <Hell></Hell>
     </>
   )
 }
